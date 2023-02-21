73dacf36882f07ce46f101e9f64edf7e45c028d9
MohammadAli <galaxy.mrz8@gmail.com>
2023-02-21 17:09:41 +0330

    update files and css file added to project

--- src/Components/Users/AddUser.js
+++ src/Components/Users/AddUser.js
@@ -1,17 +1,21 @@
 import React from 'react';
+import Card from '../UI/Card';
+import classes from './AddUser.module.css';
 
 const AddUser = () => {
     const addUserHandler = (e) => {
         e.preventDefault()
     }
     return (
-        <form onSubmit={addUserHandler}>
-            <label htmlFor='username'>Username</label>
-            <input type='text' id='username' />
-            <label htmlFor="age">Age (years)</label>
-            <input type="number" id='age' />
-            <button type="submit">Add User</button>
-        </form>
+        <Card className={classes.input}>
+            <form onSubmit={addUserHandler}>
+                <label htmlFor='username'>Username</label>
+                <input type='text' id='username' />
+                <label htmlFor="age">Age (years)</label>
+                <input type="number" id='age' />
+                <button type="submit">Add User</button>
+            </form>
+        </Card>
     );
 };
 
